1a6f4f05b8236e83915414d5d28dc60db688b708
gduboc <guillaume.duboc@ens-lyon.fr>
2018-07-31 15:39:28 -0400

    Started correct experimentations on tail symbols

--- src/parallel_tails.py
+++ src/parallel_tails.py
@@ -2,8 +2,9 @@ from multiprocessing import Process, Queue, Pool
 import numpy as np
 import os
 from math import log
-from stupid_tails import run_experiment, get_summary
+from tails import run_experiment, get_summary
 from markov import markov_chain, markov_iter_rng
+import gc
 
 
 def rng_gen(N, n):
@@ -11,8 +12,8 @@ def rng_gen(N, n):
     formula = int(N * 3 * n * log(n, 2))
     M = min(formula, 20_000_000)
 
-    # if formula > 100_000_000:
-    #     print("Chose constant")
+    if formula > 20_000_000:
+        print("Chose constant")
     # else:
     #     print("Chose formula")
 
@@ -31,11 +32,10 @@ def run_simu_mr(M, n, N):
 
     for _ in range(N):
         exp = dict()
-        exp["M"] = M
-        exp["n"] = n
 
         tail_symbols = ""
         parse = set("")
+        lnc = 0
 
         for k in range(1, n + 1):
             seq = ""
@@ -47,48 +47,85 @@ def run_simu_mr(M, n, N):
             parse.add(seq)
             tail_symbols += next(m_iter)
 
-            exp[str(k)] = seq
+            lnc += len(seq)
+            # Just keeping the total length lnc
 
-        exp["tail_symbols"] = tail_symbols
+        # Just keeping tnc
+        exp["tnc"] = tail_symbols.count("0")
+        exp["lnc"] = lnc
 
         exps.append(exp)
+
+    exps[0]["M"] = M
+    exps[0]["n"] = n
+    exps[0]["n_exp"] = len(exps)
 
     return exps
 
 
-def main(filename="test_dump.npy", n=2000, n_exp=200):
-    from timeit import default_timer as timer
-    from tqdm import tqdm
+def parallel_simu(M, ns, n_exp, d=7):
+    from progress.bar import Bar
 
-    start = timer()
-    M = markov_chain(2)
-    ns = [100, 1000, 2000]
-    n_exp = 200
-
-    d = 7
     N = n_exp // d
-
-    pbar = tqdm(total=n_exp*sum(ns))
-
-    def update(*a):
-        pbar.update(N)
 
     pool = Pool()
     sims = []
 
+    print("M is well defined : {}".format(M))
+    bar = Bar("Number of DST built", max=len(ns) * (d * N))
+
     for n in ns:
-        r = [pool.apply_async(run_simu_mr, (M, n, N), callback=update) for _ in range(d)]
+
+        print("\n=============================================\n")
+        print("Computing n = {} (max: {}), n_exp = {}".format(n, ns[-1], n_exp))
+
+        def update(*a):
+            bar.next(N)
+
+        r = [
+            pool.apply_async(run_simu_mr, (M, n, N), callback=update) for _ in range(d)
+        ]
+
         exps = sum([x.get() for x in r], [])
         sims.append(get_summary(exps))
 
-    pbar.close()
+        print(
+            "\nAverage total path length: {}".format(
+                sims[-1][1]["variables"]["mean_lnc"]
+            )
+        )
+        gc.collect()
+
+    bar.finish()
+
+    return sims
+
+
+if __name__ == "__main__":
+
+    from timeit import default_timer as timer
+    from progress.bar import Bar
+    import sys
+
+    start = timer()
+    M = markov_chain(2)
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-range":
+            ns = list(
+                range(int(input("a = ")), int(input("b = ")), int(input("step = ")))
+            )
+            n_exp = int(input("n_exp = "))
+
+    else:
+        ns = [100, 1000, 2000]
+        n_exp = 300
+
+    resu = parallel_simu(M, ns, n_exp)
 
     end = timer()
     print("Time taken:", end - start)
+    # np.save("tails_datas/" + filename, resu)
+    filename = "dummy"
     np.save("tails_datas/" + filename, resu)
-
-if __name__ == "__main__":
-    import sys
-
-    if sys.argv[1] == '-s':
-        main(sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+    print("Saved in <tails_datas/{}>".format(filename))
